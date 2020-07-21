3a8f099f34fccff34dac0878170f6d99eb150aba
Dimitar Jilanov <djilanov@gmail.com>
2020-07-22 00:35:46 +0300

    Add some logic

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const ProxyList = require('free-proxy');
 const proxyList = new ProxyList();
 const http = require('http');
+const HttpsProxyAgent = require('https-proxy-agent');
+var https = require('https');
 const url = require('url');
 const tls = require("tls");
 tls.DEFAULT_ECDH_CURVE = "auto"
-var HttpsProxyAgent = require('https-proxy-agent');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {
@@ -33,33 +34,29 @@ const sendRequest = async (res) => {
             throw new Error(error);
         }
     }
-
+    if(!data) {
+        sendRequest();
+        return;
+    }
     console.log(`connected to: http://${data.ip}:${data.port}`)
-    let agent = null;
-    try {
-        agent = new HttpsProxyAgent({
-            host: data.ip,
-            port: data.port
-        });
-    } catch(err) {
-        console.log('Proxy Err: ', err);
-    }
+    let options = {
+        url: 'https://thevoice.bg/charts/vote/',
+        method: 'POST',
+        proxy: new HttpsProxyAgent(data.url),
+        // headers: {
+        //     'Content-Type': 'application/x-www-form-urlencoded',
+        //     'Content-Length': Buffer.byteLength(post_data)
+        // }
+    };
     
-    try {
-        http.request({
-            host: 'thevoice.bg',
-            port: 443,
-            method: 'POST',
-            path: '/charts/vote/',
-            agent: agent
-        }, (res) => {
-            res.on('data', function (data) {
-                console.log(data.toString());
-            });
-        });
-    } catch(err) {
+    let request = http.request(options, (res) => {
+        console.log('"response" event!', res.headers);
+        res.pipe(process.stdout);
+    });
+    request.on('error', function(err) {
         console.log('Err: ', err);
-    }
+        sendRequest();
+    });
 
     console.log('data: ', data);
 }
